56f2c8df994a64cec565c50ad5813d02395b5c63
Lotuslinny <67099000+Lotuslinny@users.noreply.github.com>
2020-08-31 16:59:39 +0200

    Add files via upload
    M03_L12_Variables_Datatypes

---
+++ M03_L11_Js_Variables_Datatypes_Opdracht1/Javascript/script.js
@@ -0,0 +1,43 @@
+console.log('Hello World');
+//let name = 'Lin Ny';
+//console.log(name);
+let name = 'if';
+console.log(name);
+//let if= "iets";
+//console.log(if);
+// if can not be a variable, beacause it is not a resevered keyword.
+
+//Antwoord 7 : het kan allebei met twee strins en 1.
+let uitkomst = '4+4';
+console.log * (uitkomst);
+//let uitkomst = '4' + '4';
+//console.log * (uitkomst);
+
+console.log(5 + 5);
+
+console.log(6 - 5);
+
+console.log(6 * 6);
+
+console.log(36 / 3);
+
+console.log(65 % 5);
+
+typeof 40 === 'number';
+console.log(40);
+
+typeof 'veertig' === 'string';
+console.log('veertig');
+
+
+function myFunction() {
+  var y = 5;
+  var x = y + 2;
+  document.getElementById("addition").innerHTML = x;
+}
+function myFunction() {
+  var y = 5;
+  var x = y - 2;
+  document.getElementById("subtraction").innerHTML = x;
+}
+
